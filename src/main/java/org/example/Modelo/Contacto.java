d696f7b4f69746ae38dd1ec9c4b7cc7dfb01a1ee
jhonpraxedesumg <jsalazarz@miumg.edu.gt>
2024-05-18 09:04:48 -0600

    TreeAgendda

---
+++ src/main/java/org/example/Modelo/Contacto.java
@@ -0,0 +1,28 @@
+package org.example.Modelo;
+
+public class Contacto {
+    private String nombre;
+    private String telefono;
+
+    public Contacto(String nombre, String telefono) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    //hacer el set para nombre
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+}
---
+++ src/main/java/org/example/Vista/Main.java
@@ -0,0 +1,36 @@
+package org.example.Vista;
+
+import org.example.Modelo.Contacto;
+import org.example.servicio.Agenda;
+
+public class Main {
+    public static void main(String[] args) {
+        Agenda agenda = new Agenda();
+
+        // Agregar contactos
+        agenda.agregarContacto("Mario", "123456789");
+        agenda.agregarContacto("Link", "987654321");
+        agenda.agregarContacto("Peach", "456789123");
+
+        // Mostrar contactos
+        System.out.println("Contactos en la agenda:");
+        agenda.mostrarContactos();
+
+        // Buscar un contacto
+        System.out.println("\nBuscando el contacto de Link:");
+        Contacto contacto = agenda.buscarContacto("Link");
+        if (contacto != null) {
+            System.out.println("Nombre: " + contacto.getNombre() + ", Teléfono: " + contacto.getTelefono());
+        } else {
+            System.out.println("Contacto no encontrado.");
+        }
+
+        // Eliminar un contacto
+        System.out.println("\nEliminando el contacto de Peach.");
+        agenda.eliminarContacto("Peach");
+
+        // Mostrar contactos después de la eliminación
+        System.out.println("Contactos en la agenda después de eliminar a Alice:");
+        agenda.mostrarContactos();
+    }
+}
