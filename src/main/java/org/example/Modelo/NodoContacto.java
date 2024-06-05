7cd959871f0443799693088420f1c9e6437325c0
jhonpraxedesumg <jsalazarz@miumg.edu.gt>
2024-06-04 21:17:00 -0600

    TreeAgendda

--- src/main/java/org/example/Modelo/NodoContacto.java
+++ src/main/java/org/example/Modelo/NodoContacto.java
@@ -1,6 +1,8 @@
 package org.example.Modelo;
 
-public class NodoContacto {    //implements serealizable
+import java.io.Serializable;
+
+public class NodoContacto implements Serializable {    //implements serealizable
     private Contacto contacto;
     private NodoContacto izdo;
     private NodoContacto dcho;
